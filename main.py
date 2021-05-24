26efeb0b2bf49f977e1aac86dfef2afab6453f64
Franco Serio <francoagustinserio@gmail.com>
2021-05-24 00:08:10 -0300

    Done AF percentage and days

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import arrow
+import tweepy
+import time
+import schedule
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def job():
+    finalMandato = arrow.Arrow(2023, 12, 10)
+    comienzoMandato = arrow.Arrow(2019, 12, 10)
+    diasQueFaltanDeMandato = ( finalMandato - arrow.now() ).days
+    diasTotalesDeMandato = (finalMandato - comienzoMandato).days
+    diasQuePasaronDeMandato = (arrow.now() - comienzoMandato).days
+    percentageDone = round( ( diasQuePasaronDeMandato / diasTotalesDeMandato ) * 100, 2 )    
+    auth = tweepy.OAuthHandler(os.getenv('TWITTER_API_KEY'), os.getenv('TWITTER_API_SECRET'))
+    auth.set_access_token(os.getenv('TWITTER_ACCESS_TOKEN_KEY'), os.getenv('TWITTER_ACCESS_TOKEN_SECRET'))
+    
+    api = tweepy.API(auth)
+    api.update_status("Faltan " + str( diasQueFaltanDeMandato ) + " días para que termine el gobierno de Alberto Fernández. Ya pasó el " + str( percentageDone ) + "% de su mandato.")
+    return
+
+schedule.every().day.at("09:00").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
+
+# if __name__ == '__main__':
+#     job()
