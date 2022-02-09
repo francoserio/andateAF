d864a81e7de2bcf944aafd28d51eaab429a327b7
Franco Serio <francoagustinserio@gmail.com>
2022-02-09 09:03:36 -0300

    Add request argument to main function

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def sendTweet():
+def sendTweet(request):
     finalMandato = arrow.Arrow(2023, 12, 10)
     comienzoMandato = arrow.Arrow(2019, 12, 10)
     diasQueFaltanDeMandato = ( finalMandato - arrow.now() ).days
